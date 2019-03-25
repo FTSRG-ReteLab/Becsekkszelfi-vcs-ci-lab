7a146ca10e0b06e2914c5488616fecaed69f6873
meres <meres@ubuntu>
2019-03-25 09:55:20 -0700

    tests

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -7,14 +7,31 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
-    @Before
-    public void before() {
-        // TODO Add initializations
-    }
+@Test
+public void test1() {
+	overrideSpeedLimit(-1);
+	Assert.assertTrue(user.getAlarmState);
+}
 
-    @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
-    }
+@Test
+public void test2() {
+	overrideSpeedLimit(510);
+	Assert.assertTrue(user.getAlarmState);
+}
+
+@Test
+public void test3() {
+	overrideSpeedLimit(150);
+	overrideSpeedLimit(50);
+	Assert.assertTrue(user.getAlarmState);
+}
+
+@Test
+public void test4() {
+	overrideSpeedLimit(10);
+	Assert.assertEquals(speedLimit, 10);
+}
+	
+  
     
  
